3712acf6b3cbb3fac20e8ce4bb434e5990591115
Khushi Lalchandani <natureloving9999@gmail.com>
2025-09-16 16:35:16 +0530

    conditional rendering of sidebar and mainwindow

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,53 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login/Login';
+import ChatWindow from './pages/chat/ChatWindow';
+// import ProtectedRoutes from './auth/auth.guard.js';
 import React, { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase/firebase';
 import { setupPresence } from './utils/presence';
+import CreateGroupModal from './components/CreateGroupModal';
+import MainWindow from './pages/chat/MainWindow';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Login from './pages/Login/Login';
-import ChatWindow from './pages/chat/ChatWindow';
-import ProtectedRoutes from './auth/auth.guard';
+
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log(user);
       setIsAuthenticated(!!user);
-      setLoading(false);
+
       if (user) {
         setupPresence(user.uid, true);
       }
     });
+
     const handleBeforeUnload = () => {
       const currentUser = auth.currentUser;
       if (currentUser) {
         setupPresence(currentUser.uid, false);
       }
     };
+
+
+    //problem is protecting routes. might need upgrading of version.
     window.addEventListener('beforeunload', handleBeforeUnload);
+
     return () => {
       unsubscribe();
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-
-        <Route element={<ProtectedRoutes isAuthenticated={isAuthenticated} />}>
-          <Route path="/chat" element={<ChatWindow />} />
-          <Route path="/chat/:chatId" element={<ChatWindow />} />
-          <Route path="/chat/group/:groupId" element={<ChatWindow />} />
-        </Route>
+        <Route path="/chat" element={<ChatWindow />} />
+        <Route path="/chat/:chatId" element={<ChatWindow />} />
+        <Route path="/chat/group/:groupId" element={<ChatWindow />} />
       </Routes>
       <ToastContainer
         position="top-center"
@@ -59,12 +61,13 @@ function App() {
         pauseOnHover
         theme="dark"
         toastStyle={{
-          background: '#1F1F1F',
-          color: '#FBBF24',
-          border: '1px solid #EAB308'
+          background: '#1f1f1f',
+          color: '#fbbf24',
+          border: '1px solid #eab308'
         }}
       />
     </BrowserRouter>
   );
 }
+
 export default App;
